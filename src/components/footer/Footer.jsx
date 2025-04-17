c2256230509b884a8ec4a6f8c45a3aeee7c11e29
Plinio Prado <plinioprado01@gmail.com>
2025-04-17 12:31:34 -0700

    footer ok

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+  return (
+    <footer>
+        <div className="container">
+            <span>&copy; Plinio Prado. All rights reserved.</span>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
