e3f1c638cffd85979f0cbec5f071544ab4bbc477
Plinio Prado <plinioprado01@gmail.com>
2025-05-28 11:57:37 -0700

    better error messaging

--- src/data/request.js
+++ src/data/request.js
@@ -1,29 +1,23 @@
 import config from "../config.json";
 
 export async function get(path, api_key, qString) {
-  try {
-    const url = `http://localhost:8000${config.url_base_api}${path}${
-      qString || ""
-    }`;
-    const result = await doRequest(url, "GET", api_key);
+  const url = `${config.url_base_api}${path}${qString || ""}`;
+  const result = await doRequest(url, "GET", api_key);
 
-    return {
-      data: result.data,
-      format: result.format,
-    };
-  } catch (err) {
-    return err;
-  }
+  return {
+    data: result.data,
+    format: result.format,
+  };
 }
 
 export async function post(qPath, api_key, data) {
-  const url = `http://localhost:8000${config.url_base_api}${qPath}`;
+  const url = `${config.url_base_api}${qPath}`;
   const result = await doRequest(url, "POST", api_key, data);
   return result;
 }
 
 export async function put(path, api_key, data) {
-  const url = `http://localhost:8000${config.url_base_api}${path}`;
+  const url = `${config.url_base_api}${path}`;
   const result = await doRequest(url, "PUT", api_key, data);
   return result;
 }
@@ -49,7 +43,15 @@ const doRequest = async (url, method, api_key, body) => {
   const response = await fetch(url, options);
 
   if (response.status !== 200) {
-    throw new Error(`Invalid response status: ${response.status}`);
+    const responseBody = await response.text();
+    const bodyObj = responseBody ? JSON.parse(responseBody) : {};
+    const message = bodyObj.message.replace("Error: ", "") || "";
+
+    if (!responseBody || responseBody === "{}") {
+      throw new Error(`Response error ${response.status}: ${message}`);
+    }
+
+    throw new Error(`Response error ${response.status}: ${message}`);
   }
 
   const json = await response.json();
