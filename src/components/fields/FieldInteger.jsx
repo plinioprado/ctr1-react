6b48655694ff761385348f4bec5c89164f94ec22
Plinio Prado <plinioprado01@gmail.com>
2025-07-23 09:23:48 -0700

    select

--- src/components/fields/FieldInteger.jsx
+++ src/components/fields/FieldInteger.jsx
@@ -1,3 +1,5 @@
+import InputInteger from "./InputInteger";
+
 function FieldInteger({ data_field, format_field, handleChange, is_new }) {
   const handleIntegerChange = (e) => {
     const val = e.target.value;
@@ -5,21 +7,12 @@ function FieldInteger({ data_field, format_field, handleChange, is_new }) {
   };
 
   return (
-    <div className={`col-md-${format_field.md}`} key={format_field.name}>
-      <label htmlFor={format_field.name}>{format_field.label}</label>
-      <input
-        type={format_field.type}
-        className="form-control"
-        id={format_field.name}
-        name={format_field.name}
-        value={data_field}
-        onChange={handleIntegerChange}
-        disabled={format_field.primary}
-        is_new={is_new}
-        style={{ textAlign: "right" }}
-        readOnly={format_field.readOnly}
-      />
-    </div>
+    <InputInteger
+      data_field={data_field}
+      format_field={format_field}
+      handleChange={handleIntegerChange}
+      is_new={is_new}
+    />
   );
 }
 
