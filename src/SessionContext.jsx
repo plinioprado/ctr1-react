29e3b70a8cd2a66fc29d78057be5dfccd60dd511
Plinio Prado <plinioprado01@gmail.com>
2025-07-30 08:54:04 -0700

    page reload keeps session (#37)

--- src/SessionContext.jsx
+++ src/SessionContext.jsx
@@ -3,12 +3,29 @@ import { createContext, useState } from "react";
 const SessionContext = createContext();
 
 const SessionContextProvider = ({ children }) => {
-  const [session, setSession] = useState(null);
+  const [sessionState, setSessionState] = useState(null);
+
+  const setSession = (newSession) => {
+    if (newSession) {
+      localStorage.setItem("session", JSON.stringify(newSession));
+    } else {
+      localStorage.removeItem("session");
+    }
+    setSessionState(newSession);
+  };
+
+  const getSession = () => {
+    if (sessionState) {
+      return sessionState;
+    } else {
+      return JSON.parse(localStorage.getItem("session"));
+    }
+  };
 
   const shop = {
     setSession: setSession,
-    session: session,
-    api_key: session ? session.user.api_key : "",
+    session: getSession(),
+    api_key: getSession() ? getSession().user.api_key : "",
   };
 
   return (
