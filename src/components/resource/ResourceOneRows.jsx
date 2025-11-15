ee13a4d95bdd86f0fb4b2a355b3717c23724625f
Plinio Prado <plinioprado01@gmail.com>
2025-11-14 17:07:00 -0800

    wip

--- src/components/resource/ResourceOneRows.jsx
+++ src/components/resource/ResourceOneRows.jsx
@@ -1,18 +1,31 @@
 function ResourceOneRows({ format_rows, data, onDataChange, is_new }) {
-  console.log(format_rows.columns);
+  const data_rows = data[format_rows.name] || [];
+  const format_columns = format_rows.columns || [];
+  console.log(data_rows);
   return (
     <div className={`col-md-${format_rows.md}`} key={format_rows.name}>
       <div className="row">
-        {format_rows.columns &&
-          format_rows.columns.map((format_column) => (
+        {format_columns.map((format_column) => (
+          <div
+            className={`col-md-${format_column.md} fw-bold`}
+            key={format_column.name}
+          >
+            {format_column.label}
+          </div>
+        ))}
+      </div>
+      {data_rows.map((data_row, row_index) => (
+        <div className="row" key={row_index}>
+          {format_columns.map((format_column) => (
             <div
-              className={`col-md-${format_column.md} fw-bold`}
+              className={`col-md-${format_column.md}`}
               key={format_column.name}
             >
-              {format_column.label}
+              {data_row[format_column.name]}
             </div>
           ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
