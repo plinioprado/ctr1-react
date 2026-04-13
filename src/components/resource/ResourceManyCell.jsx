2bf3d1c17b02fc2cfd4819e3452997de7c020221
Plinio Prado <plinioprado01@gmail.com>
2026-04-13 14:59:17 -0700

    empty cell

--- src/components/resource/ResourceManyCell.jsx
+++ src/components/resource/ResourceManyCell.jsx
@@ -1,7 +1,7 @@
 function ResourceGridCell({ value_cell, format_cell, route_url }) {
   const formatAmount = (val) => {
     let text = "";
-    if (val !== "" && val !== undefined) {
+    if (!["", undefined, null].includes(val)) {
       text = (val / 100).toLocaleString("en", {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
@@ -19,21 +19,21 @@ function ResourceGridCell({ value_cell, format_cell, route_url }) {
     format_cell.type == "amount"
       ? formatAmount(value_cell)
       : format_cell.type == "percentage"
-      ? formatPercentage(value_cell)
-      : format_cell.type == "boolean"
-      ? value_cell
-        ? "Yes"
-        : "No"
-      : value_cell;
+        ? formatPercentage(value_cell)
+        : format_cell.type == "boolean"
+          ? value_cell
+            ? "Yes"
+            : "No"
+          : value_cell;
 
   const cell_class =
     format_cell.type == "integer"
       ? " integer"
       : format_cell.type == "amount"
-      ? " amount"
-      : format_cell.type == "percentage"
-      ? " percentage"
-      : "";
+        ? " amount"
+        : format_cell.type == "percentage"
+          ? " percentage"
+          : "";
   const col = format_cell.md ? `col-md-${format_cell.md}` : "col";
   return (
     <>
